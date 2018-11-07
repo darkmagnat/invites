0987c17b97967a6610582221559c86e2e98cb20c
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-11-07 18:37:13 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+	client.user.setGame(`-Universe Town`1K-Soon......`,'https://www.twitch.tv/v5bz');
+console.log("Discord Bot By So.?");
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+
+
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (author) {
+  return author.send(' ** [ https://discord.gg/MfsDEhC ]      [قلوبهمّ تفيض احتراما وتقديرا لك♡الأشخاص  يبحُثون عن مكان جَميل والبّعض يجعل المكان اجمَل:black_heart: . حياكم الله معناالدعووة خاصة لك...:revolving_hearts:]  ** ')
+
+}).catch(console.error)
+})
+
+client.login(process.env.BOT_TOKEN);
